1f9b782695a5c70bdc90f7d0b4cb1f2584bcadf4
Ikuo Terado <eqobar@gmail.com>
2014-11-23 00:03:24 +0900

    Add event model

---
+++ db/migrate/20141122145543_create_events.rb
@@ -0,0 +1,16 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :title,        null: false
+      t.text :description
+      t.datetime :start_time, null: false
+      t.datetime :end_time,   null: false
+      t.string :place,        null: false
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+
+    add_index :events, :user_id
+  end
+end
