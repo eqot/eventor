4e3f0b6bab347ab0c5678db173440f3191e85fc8
Ikuo Terado <eqobar@gmail.com>
2014-11-23 01:55:09 +0900

    Show event list

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,5 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
 
-  has_many :created_events, class_name: 'Event', foreign_key: :owner_id
-  has_many :tickets
-  has_many :registered_events, through: :tickets, source: :event
+  has_many :events
 end
