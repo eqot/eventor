f0a93992aea4c0c4667f5707dea76ad8bc4e7644
Ikuo Terado <eqobar@gmail.com>
2014-11-23 00:03:24 +0900

    Add event model

---
+++ spec/factories/events.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :event do
+    title "MyString"
+description "MyText"
+start_time "2014-11-22 23:55:43"
+end_time "2014-11-22 23:55:43"
+place "MyString"
+user_id 1
+  end
+
+end
