307be893d2832f1215e537c2cb3b07012874c524
Ikuo Terado <eqobar@gmail.com>
2014-11-23 02:20:23 +0900

    Refactor code with helper to show timeslot

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,8 @@
 module EventsHelper
+
+  def show_timeslot(start_time, end_time)
+    timeslot = start_time.strftime('%Y/%m/%d %H:%M') + ' - ' + end_time.strftime('%Y/%m/%d %H:%M')
+    content_tag(:span, timeslot)
+  end
+
 end
