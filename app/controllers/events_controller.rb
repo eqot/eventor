aad2097fdfcba244a77ef783387f7dcf361cb66e
Ikuo Terado <eqobar@gmail.com>
2014-11-23 02:52:31 +0900

    Support creating event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,25 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: 'Created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :title, :description, :start_time, :end_time, :place
+    )
+  end
+
 end
