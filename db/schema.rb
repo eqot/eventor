ce7dcc66e98ee0146f46e26abccae52494d87450
Ikuo Terado <eqobar@gmail.com>
2014-11-23 00:03:24 +0900

    Add event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110152005) do
+ActiveRecord::Schema.define(version: 20141122145543) do
+
+  create_table "events", force: true do |t|
+    t.string   "title",       limit: 255,   null: false
+    t.text     "description", limit: 65535, null: false
+    t.datetime "start_time",                null: false
+    t.datetime "end_time",                  null: false
+    t.string   "place",       limit: 255,   null: false
+    t.integer  "user_id",     limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  limit: 255, default: "", null: false
