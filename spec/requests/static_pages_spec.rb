8791ade834dbab2a6b5f4fb947f5e6b810727d0e
Ikuo Terado <eqobar@gmail.com>
2014-11-22 23:40:52 +0900

    Replace title to Congreg

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
-
 describe 'Static pages' do
   describe 'Top page' do
-    it "should have the content 'rails-base'" do
+    it "should have the content 'Eventor'" do
       visit root_path
-      expect(page).to have_content('rails-base')
+      expect(page).to have_content('Eventor')
     end
   end
 end
