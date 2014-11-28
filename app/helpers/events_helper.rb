8bf9408704c68b04ee0ff21125b7e2734e200b15
Ikuo Terado <eqobar@gmail.com>
2014-11-29 00:44:24 +0900

    Change date format

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,7 +1,7 @@
 module EventsHelper
 
   def show_timeslot(start_time, end_time)
-    timeslot = start_time.strftime('%Y/%m/%d %H:%M') + ' - ' + end_time.strftime('%Y/%m/%d %H:%M')
+    timeslot = start_time.strftime('%m/%d/%Y %H:%M') + ' - ' + end_time.strftime('%H:%M')
     content_tag(:span, timeslot)
   end
 
