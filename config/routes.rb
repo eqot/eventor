d9ae4ec15cca84bc3628e260bda8f5db71721f08
Ikuo Terado <eqobar@gmail.com>
2014-12-10 00:50:14 +0900

    Customize kaminari for creating friendly URLs and caching

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :events do
+  concern :paginatable do
+    get '(page/:page)', :action => :index, :on => :collection, :as => ''
+  end
+
+  resources :events, :concerns => :paginatable do
     resources :tickets
   end
 
