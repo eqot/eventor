01a36033f002c9a4a9e2ed8d394badd2fbb87b95
Ikuo Terado <eqobar@gmail.com>
2014-12-10 00:17:41 +0900

    Change name to Eventor

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,9 @@ describe "Static pages" do
 
   describe "Top page" do
 
-    it "should have the content 'Congreg'" do
+    it "should have the content 'Eventor'" do
       visit root_path
-      expect(page).to have_content('Congreg')
+      expect(page).to have_content('Eventor')
     end
   end
 end
