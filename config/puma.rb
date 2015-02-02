cda83e875775805a3dbaf53d33bdf7592fba0834
Ikuo Terado <eqobar@gmail.com>
2015-02-03 00:03:52 +0900

    Minor change

--- config/puma.rb
+++ config/puma.rb
@@ -4,5 +4,5 @@ threads( 0, 16 )
 bind        "tcp://0.0.0.0:3000"
 # bind      "unix:#{rails_root}/tmp/sockets/puma.sock"
 pidfile     "#{rails_root}/tmp/pids/puma.pid"
-state_path  "#{rails_root}/tmp/puma.state"
+state_path  "#{rails_root}/tmp/pids/puma.state"
 activate_control_app
