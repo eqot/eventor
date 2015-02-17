f6fa593490753d2d1eceaf35c83394db222dd425
Ikuo Terado <eqobar@gmail.com>
2015-02-18 02:01:25 +0900

    Modify models for events

---
+++ app/models/event_invitation.rb
@@ -0,0 +1,19 @@
+class EventInvitation < ActiveRecord::Base
+  belongs_to :event
+
+  # validate :start_time_should_be_before_end_time
+
+  def iso8601
+    self.start_time = start_time.iso8601
+    self.end_time = end_time.iso8601
+  end
+
+  private
+
+  def start_time_should_be_before_end_time
+    return unless start_time && end_time
+    return unless start_time >= end_time
+
+    errors.add(:start_time, 'should be before end time')
+  end
+end
