262ccf79869aaf4d97f1a3e26f6d060256731527
Ikuo Terado <eqobar@gmail.com>
2015-03-07 01:29:39 +0900

    Support max number of attendees

--- app/models/event_invitation.rb
+++ app/models/event_invitation.rb
@@ -1,6 +1,7 @@
 class EventInvitation < ActiveRecord::Base
   belongs_to :event
 
+  validates :max_attendees, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validate :start_time_should_be_before_end_time
 
   private
