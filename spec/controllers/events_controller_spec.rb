1cdcb233a6e069354712c294410de8dbf47c82f7
Ikuo Terado <eqobar@gmail.com>
2015-03-09 01:29:19 +0900

    Add test event controller

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
+  describe 'Get #show' do
+    it "assigns the requested event to @event" do
+      user = create(:user)
+      sign_in user
+
+      event = create(:event)
+      get :show, id: event
+      expect(assigns(:event)).to eq event
+    end
+  end
 end
