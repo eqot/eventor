cadfebdeb174c9cd1cb70dd6accf5ef570f14c38
Ikuo Terado <eqobar@gmail.com>
2015-03-10 01:33:52 +0900

    Minor change

--- spec/factories/event_invitations.rb
+++ spec/factories/event_invitations.rb
@@ -4,7 +4,6 @@ FactoryGirl.define do
   factory :event_invitation do
     sequence(:start_time) { |n| Time.now + n.days }
     end_time { start_time + 1.hour }
-    max_attendees 4
 
     location { Faker::Address.city }
   end
