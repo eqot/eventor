9b1158806bd5c75fae06c2c67b2ce521c9a5ee42
Ikuo Terado <eqobar@gmail.com>
2015-03-12 01:14:43 +0900

    Fix rails_best_practices issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,30 +15,8 @@ class UsersController < ApplicationController
       format.html
 
       format.ics do
-        @calendar = make_calendar(@events, request.host_with_port)
+        @calendar = view_context.make_calendar(@events, request.host_with_port)
         render text: @calendar.export
-      end
-    end
-  end
-
-  private
-
-  def make_calendar(events, host)
-    RiCal.Calendar do
-      events.each do |ev|
-        url = Rails.application.routes.url_helpers.event_url(ev, host: host)
-
-        event do
-          summary            ev.title
-          description        ev.description
-          dtstart            ev.start_time
-          dtend              ev.end_time
-          location           ev.place
-          organizer_property ";CN=\"#{ev.owner.name}\":mailto:#{ev.owner.email}"
-          uid                url
-          url                url
-          add_attendee       ev.attendees.collect(&:name).join(', ')
-        end
       end
     end
   end
